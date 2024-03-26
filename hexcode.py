fe66fa71247aefc4b92dd0912b381b937a0651c0
SouthSide.73 <99437405+SouthSide73@users.noreply.github.com>
2024-03-26 19:54:42 +0400

    hexcode.py

--- hexcode.py
+++ hexcode.py
@@ -1,29 +1,28 @@
 import re
 def find_hex_nums(text):
 	# Поиск шестнадцатеричных чисел с F в любом месте и кол-ве
-	reg = r'\b(?:0x)?(?:[0-9a-eA-E]*F[0-9a-fA-F]*|[0-9a-fA-F]*F[0-9a-eA-E]*)\b'
+	reg = r'\b(?:0x)?(?:F[0-9a-eA-E]*\b|[0-9a-eA-E]*F)\b'
 	hex_num = re.findall(reg, text)
 	return hex_num
 def main():
+	count_hex = 0
 	with open('data.txt', 'r') as file:
 		text = file.read()
 	print("Входные данные:")
 	print(text)
 	hex_numbers = find_hex_nums(text)
 	if hex_numbers:
-		# Отфильтровать числа с F в начале или в конце
-		filter = [num for num in hex_numbers if num.count('F') == 1 and (num[0] == 'F' or num[-1] == 'F')]
-		if filter:
-			# Найти максимальное число
-			max_hex = max(int(num, 16) for num in filter)
-			max_hex_str = hex(max_hex)
-			print("\nМаксимальное число с одной F в начале или в конце:", str(max_hex_str).upper()[2:])
-			# Определить количество чисел
-			count_hex = len(filter)
-			print("Количество таких чисел:", count_hex)
-		else:
-			print("\nШестнадцатеричные числа, подходящие к условию, не найдены.")
+		# Найти максимальное число
+		max_hex = max(int(num, 16) for num in hex_numbers)
+		max_hex_str = hex(max_hex)
+		print("\nМаксимальное число с одной F в начале или в конце:", str(max_hex_str).upper()[2:])
+		# Определить количество чисел максимальных
+		count = len(hex_numbers)
+		for i in range(count):
+			if str(hex_numbers[i]) == str(max_hex_str).upper()[2:]:
+				count_hex += 1
+		print("Количество таких чисел (максимальных):", count_hex)
 	else:
-		print("\nШестнадцатеричные числа не найдены.")
+		print("\nШестнадцатеричных чисел, подходящих к условию, не найдено.")
 if __name__ == "__main__":
 	main()
